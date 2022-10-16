a4f9758080d02e3582277b8fafb5ff4797cabe44
AleSrv <112753551+AleSrv@users.noreply.github.com>
2022-10-16 21:58:46 +0200

    Update tp5.js

--- JAVASCRIPT/TP5/tp5.js
+++ JAVASCRIPT/TP5/tp5.js
@@ -3,4 +3,4 @@ let alt_metros = 1.79;
 let peso_kg= 89.5;
 let alt_redondeo_arriba = Math.ceil(alt_metros);
 let peso_redondeo_abajo = Math.floor(peso_kg);
-let valor_max = Number.MAX_VALUE + 1;
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
