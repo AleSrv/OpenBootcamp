dcd4af41ce0702769f062210aec3af3ebc81399e
AleSrv <112753551+AleSrv@users.noreply.github.com>
2022-11-09 10:30:04 +0100

    Update tarea08.js

--- JAVASCRIPT/TP08/tarea08.js
+++ JAVASCRIPT/TP08/tarea08.js
@@ -1,47 +1,16 @@
 //TAREA 08 
 
-function verdadero(){
+function devuelveTrue() {
     return true
 }
 
-//
-
-function tiempo (){
-console.log("Hola soy una promesa")
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
 }
 
-//
-
-const miAsinc = new Promise((resolve, reject) => {
-if(verdadero()===true){
-    resolve()
-}else{
-    reject()
-}
-})
-
-miAsinc
-.then(()=>setTimeout(tiempo,5000))
-.catch(()=>console.log("ERROR"))
-.finally(()=>console.log("Yo me ejecuto siempre"))
-
-//
-
-function* generapar(){
-    let id=0;
-    while(true){
-        id = id+2;
-        if (id===10){
-            return id
-        }
-        yield id // Esperando hasta que vuelva a llamar
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
     }
 }
-
-const gen = generapar();
-
-console.log(gen.next().value) // 2
-console.log(gen.next().value) // 4
-console.log(gen.next().value) // 6
-console.log(gen.next().value) // 8
-console.log(gen.next().value) // 10
